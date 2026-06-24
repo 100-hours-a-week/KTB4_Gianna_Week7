339871bded5a7686b70433d0b62ba29503c67b33
Stella <yeobinlee1210@gmail.com>
2026-06-24 23:18:21 +0900

    feat(boardView): 게시물 상세목록 구현 요소 구현

---
+++ src/board/board.js
@@ -0,0 +1,6 @@
+const postContainer = document.getElementsByClassName('common-container')
+
+postContainer.addEventListener('click' ,(event)=>{
+    console.log('cli')
+    window.location.href = "./boardView.html";
+});

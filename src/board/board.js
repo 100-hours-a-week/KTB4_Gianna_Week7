4c1ebfa920490dd261ff930ecf378f6021a3fb48
Stella <yeobinlee1210@gmail.com>
2026-06-28 00:58:56 +0900

    feat(board): 게시글 조회 API 연동

--- src/board/board.js
+++ src/board/board.js
@@ -1,5 +1,28 @@
+getPostsList();
+
 const postContainer = document.getElementsByClassName("common-container");
 
 postContainer[0].addEventListener('click' ,(event)=>{
     window.location.href = "./boardView/boardView.html";
 });
+
+async function getPostsList  () {
+    try{
+        const response = await fetch('http://localhost:8080/posts', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            
+        });
+
+        if (!response.ok) {
+            throw new Error('로그인 실패');
+        }
+
+        const data = await response.json();
+        console.log(data.data)
+    }catch(error){
+        console.error('로그인 중 오류 발생:', error);
+    }
+}

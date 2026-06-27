e0499f5f111a933611e58b2c829e497244d6302f
Stella <yeobinlee1210@gmail.com>
2026-06-28 00:42:06 +0900

    feat(boardWrite): 게시글 작성 API 연동 구현

--- src/board/board.js
+++ src/board/board.js
@@ -1,5 +1,5 @@
 const postContainer = document.getElementsByClassName("common-container");
-console.log(postContainer)
+
 postContainer[0].addEventListener('click' ,(event)=>{
     window.location.href = "./boardView/boardView.html";
 });
---
+++ src/board/boardWrite/boardWrite.js
@@ -0,0 +1,36 @@
+const titleInput = document.getElementById('postTitle')
+const contentInput = document.getElementById('postContent')
+const pictureInput = document.getElementById('postUploadedPicture')
+const postWriteBtn = document.getElementById('postWriteBtn')
+
+const curDate = new Date();
+
+postWriteBtn.addEventListener('click', async (event)=>{
+    const userIdCookie = await cookieStore.get('userId');
+    const userId = userIdCookie.value
+    try{
+        const response = await fetch(`http://localhost:8080/posts/${userId}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title : titleInput.value,
+                content : contentInput.value,
+                date : curDate,
+                file : pictureInput.value.length ===0 ? null : pictureInput.value
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error('로그인 실패');
+        }
+
+        const data = await response.json();
+
+        window.location.replace('../board/board.html');
+
+    }catch(error){
+        console.error('로그인 중 오류 발생:', error);
+    }
+})
